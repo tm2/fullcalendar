9d3fda508596b564bf2dc242255169b61bdf447a
Shaw Creaney <shaw.creaney@blue-zinc.com>
2014-03-31 11:38:05 +0100

    Chris' Daylight Savings Fix

--- src/agenda/AgendaDayView.js
+++ src/agenda/AgendaDayView.js
@@ -20,13 +20,14 @@ function AgendaDayView(element, calendar) {
 	
 	
 	function render(date, delta) {
+		debugger
 		if (delta) {
 			addDays(date, delta);
 			if (!opt('weekends')) {
 				skipWeekend(date, delta < 0 ? -1 : 1);
 			}
 		}
-		var start = cloneDate(date, true);
+		var start = cloneDate(date, false);
 		var end = addDays(cloneDate(start), 1);
 		t.title = formatDate(date, opt('titleFormat'));
 		t.start = t.visStart = start;
